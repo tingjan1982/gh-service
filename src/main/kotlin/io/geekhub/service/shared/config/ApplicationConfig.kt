258b9f993dde65bf3ee2858d9dc4efd45a2dfb7f
joelin <tingjan1982@gmail.com>
2018-05-20 16:53:40 +0800

    GH-28 Enable basic auth in Swagger UI.

--- src/main/kotlin/io/geekhub/service/shared/config/ApplicationConfig.kt
+++ src/main/kotlin/io/geekhub/service/shared/config/ApplicationConfig.kt
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.BasicAuth
 import springfox.documentation.service.Contact
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
@@ -61,12 +62,14 @@ class ApplicationConfig {
 
     @Bean
     fun api(): Docket {
+        val basicAuthScheme = BasicAuth("geekhub")
         return Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("io.geekhub.service"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo())
+                .securitySchemes(listOf(basicAuthScheme))
     }
 
     /**
