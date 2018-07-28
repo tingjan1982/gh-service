a62193994dc73ca04de2d92572c746c760a5d8a5
joelin <tingjan1982@gmail.com>
2018-07-28 22:35:42 +0800

    Remove client details after container teardown.

--- src/main/kotlin/io/geekhub/service/shared/authorization/OAuth2AuthorizationServerConfig.kt
+++ src/main/kotlin/io/geekhub/service/shared/authorization/OAuth2AuthorizationServerConfig.kt
@@ -12,10 +12,12 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices
 import org.springframework.security.oauth2.provider.token.TokenStore
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore
+import javax.annotation.PreDestroy
 import javax.sql.DataSource
 
 /**
@@ -79,6 +81,16 @@ class OAuth2AuthorizationServerConfig : AuthorizationServerConfigurerAdapter() {
                 .scopes("read", "write")
                 .accessTokenValiditySeconds(3600)
                 .refreshTokenValiditySeconds(3600)
+    }
+
+    @PreDestroy
+    fun deleteDefaultClientDetails() {
+
+        JdbcClientDetailsService(this.dataSource).let {
+            it.removeClientDetails("roclient")
+            it.removeClientDetails("shortlivedclient")
+            it.removeClientDetails("ghfront")
+        }
     }
 
     @Bean
