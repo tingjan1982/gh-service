e6b4606e9986f6c9fff2a70be23548f783dd2a4f
joelin <tingjan1982@gmail.com>
2021-07-05 15:57:13 +0800

    Fixed a app startup error.

--- src/main/kotlin/io/geekhub/service/auth0/service/CacheEvictionScheduler.kt
+++ src/main/kotlin/io/geekhub/service/auth0/service/CacheEvictionScheduler.kt
@@ -11,13 +11,17 @@ import org.springframework.stereotype.Component
 import java.time.Duration
 
 @Component
-class CacheEvictionScheduler(val cacheManager: CacheManager, @Value("\${app.cache.evictionInterval}") evictionInterval: Duration) {
+class CacheEvictionScheduler(val cacheManager: CacheManager) {
+
+    @Value("\${app.cache.evictionInterval}")
+    lateinit var evictionInterval: Duration
 
     companion object {
         val LOGGER: Logger = LoggerFactory.getLogger(CacheEvictionScheduler::class.java)
     }
 
-    @Scheduled(fixedDelayString = "#{evictionInterval.toMillis()}")
+    //@Scheduled(fixedDelayString = "#{evictionInterval.toMillis()}")
+    @Scheduled(fixedDelay = 12 * 60 * 60 * 1000)
     fun evictManagementTokenCache() {
         LOGGER.info("Evicting management token cache after the specified delay")
 
